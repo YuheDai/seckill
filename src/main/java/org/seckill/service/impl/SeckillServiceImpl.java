153d2bf18b21e40853fa8f574f6ec015a1999ffc
daiyuhe <dai.zycc@qq.com>
2018-12-22 18:32:40 +0800

    seckill

---
+++ src/main/java/org/seckill/service/impl/SeckillServiceImpl.java
@@ -0,0 +1,121 @@
+/**
+ * Copyright (C), 2018-2018, 代雨鹤
+ */
+package org.seckill.service.impl;
+
+import org.seckill.dao.SeckillDao;
+import org.seckill.dao.SuccessKilledDao;
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.entity.Seckill;
+import org.seckill.entity.SuccessKilled;
+import org.seckill.enums.SeckillStateEnum;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.seckill.exception.SeckillException;
+import org.seckill.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.DigestUtils;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author DaiYuHe
+ * @date 2018/12/10
+ */
+@Service
+public class SeckillServiceImpl implements SeckillService {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private SeckillDao seckillDao;
+
+    @Autowired
+    private SuccessKilledDao successKilledDao;
+
+    private final String slat = "lasqioujeu12u093eipodakdi@(#*!)#)(!qweqklemqklmdaksl";
+
+    @Override
+    public List<Seckill> getSeckillList() {
+        return seckillDao.queryAll(0, 4);
+    }
+
+    @Override
+    public Seckill getById(long seckillId) {
+        return seckillDao.queryById(seckillId);
+    }
+
+    @Override
+    public Exposer exportSeckillUrl(long seckillId) {
+        Seckill seckill = seckillDao.queryById(seckillId);
+        if (null == seckill) {
+            return new Exposer(false, seckillId);
+        }
+
+        Date startTime = seckill.getStartTime();
+        Date endTime = seckill.getEndTime();
+
+        //当前时间
+        Date nowTime = new Date();
+
+        if (nowTime.getTime() < startTime.getTime()
+                || nowTime.getTime() > endTime.getTime()) {
+            return new Exposer(false, seckillId, nowTime.getTime(), startTime.getTime(), endTime.getTime());
+        }
+
+        String md5 = getMD5(seckillId);
+        return new Exposer(true, md5, seckillId);
+
+    }
+
+    private String getMD5(long seckillId) {
+        String base = seckillId + "/" + slat;
+        String md5 = DigestUtils.md5DigestAsHex(base.getBytes());
+        return md5;
+    }
+
+    @Override
+    @Transactional
+    public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException, SeckillCloseException, RepeatKillException {
+        if (null == md5 || !md5.equals(getMD5(seckillId))) {
+            throw new SeckillException("seckillId data rewrite");
+        }
+
+        //执行秒杀逻辑:减库存 + 记录购买行为
+        Date nowTime = new Date();
+
+        try {
+            int updateCount = seckillDao.reduceNumber(seckillId, nowTime);
+            if (updateCount <= 0) {
+                //没有更新到记录, 秒杀结束
+                throw new SeckillCloseException("seckill is closed");
+            } else {
+                //记录购买行为
+                int insertCount = successKilledDao.insertSuccessKilled(seckillId, userPhone);
+                //唯一: seckillId, userPhone
+                if (insertCount <= 0) {
+                    //重复秒杀
+                    throw new RepeatKillException("seckill repeated");
+                } else {
+                    //秒杀成功
+                    SuccessKilled successKilled = successKilledDao.queryByIdWithSeckill(seckillId, userPhone);
+                    return new SeckillExecution(seckillId,SeckillStateEnum.SUCCESS, successKilled);
+                }
+            }
+        } catch (SeckillCloseException e1) {
+            throw e1;
+        } catch (RepeatKillException e2) {
+            throw e2;
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            //所有编译期异常转化为运行期异常
+            throw new SeckillException("seckill inner error:" + e.getMessage());
+        }
+    }
+}
